4e3f96271bd0a4b4ccb4a636d702831cc4a2f170
stepper1029 <olivian.stepper@gmail.com>
2023-11-09 22:51:57 -0500

    Changes by oliviaromito

--- test/cs3500/reversi/strategy/PackagePrivateStrategyTests.java
+++ test/cs3500/reversi/strategy/PackagePrivateStrategyTests.java
@@ -3,23 +3,41 @@ package cs3500.reversi.strategy;
 import org.junit.Assert;
 import org.junit.Test;
 
-import cs3500.reversi.Reversi;
 import cs3500.reversi.model.ReadOnlyModel;
 import cs3500.reversi.model.ReversiCreator;
-import cs3500.reversi.strategy.TopLeftComparator;
 
 /**
  * Tests for package-private methods/ classes in the strategy package.
  */
 public class PackagePrivateStrategyTests {
   // test TopLeftComparator
+
+  // model to use for retrieving cells
   ReadOnlyModel model;
-  // initializes the model to be able to retrieve cells
+
+  // initializes the model to a board of size 4
   private void initModel() {
     this.model = ReversiCreator.create(4);
   }
+
   @Test
   public void testTopLeftComparatorEqual() {
-    Assert.assertTrue(new TopLeftComparator().compare());
+    this.initModel();
+    Assert.assertEquals(new TopLeftComparator().compare(this.model.getCellAt(1, 2),
+            this.model.getCellAt(1, 2)), 0);
+    Assert.assertEquals(new TopLeftComparator().compare(this.model.getCellAt(3, 3),
+            this.model.getCellAt(3, 3)), 0);
+    Assert.assertEquals(new TopLeftComparator().compare(this.model.getCellAt(5, 2),
+            this.model.getCellAt(5, 2)), 0);
+    Assert.assertEquals(new TopLeftComparator().compare(this.model.getCellAt(4, 1),
+            this.model.getCellAt(4, 1)), 0);
+  }
+
+  @Test
+  public void testNegValue() {
+    this.initModel();
+    Assert.assertTrue(new TopLeftComparator().compare(this.model.getCellAt(1, 2),
+            this.model.getCellAt(0, 0)) < 0);
+  //  Assert.assertTrue(new TopLeftComparator().compare(this.model.getCellAt(1, 3), this.model.getCellAt(4, 0)));
   }
 }
