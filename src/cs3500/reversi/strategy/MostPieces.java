3ede08134e8a21e0f7d41487c67b33d71d7b212c
oromito <oliviaromito@gmail.com>
2023-11-12 00:29:09 -0500

    Updating mostPieces strategy

--- src/cs3500/reversi/strategy/MostPieces.java
+++ src/cs3500/reversi/strategy/MostPieces.java
@@ -1,7 +1,9 @@
 package cs3500.reversi.strategy;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import cs3500.reversi.model.MutableModel;
 import cs3500.reversi.model.ReadOnlyModel;
@@ -9,20 +11,33 @@ import cs3500.reversi.model.DiskColor;
 import cs3500.reversi.model.ReversiCell;
 
 
+
 public class MostPieces implements FallibleReversiStrategy {
   @Override
   public List<ReversiCell> allGoodMoves(ReadOnlyModel model, DiskColor player, List<ReversiCell>
           possibleMoves) {
+    ArrayList<ReversiCell> goodMoves = new ArrayList<>();
     int highestScore = 0;
     for (ReversiCell cell : model.allPossibleMoves(player)) {
       MutableModel modelCopy = model.copy();
       modelCopy.place(cell, player);
       if (modelCopy.getScore(player) > highestScore) {
-
+        highestScore = modelCopy.getScore(player);
+        goodMoves.add(cell);
+      }
+      else if (modelCopy.getScore(player) == highestScore) {
+        goodMoves.add(cell);
       }
     }
-    return null;
-  }
+    int finalHighestScore = highestScore;
+    return goodMoves.stream()
+            .filter(cell -> {
+              MutableModel modelCopy = model.copy();
+              modelCopy.place(cell, player);
+              return modelCopy.getScore(player) == finalHighestScore;
+            })
+            .collect(Collectors.toList());
+    }
 
   @Override
   public Optional<ReversiCell> bestPotentialMove(ReadOnlyModel model, DiskColor player,
